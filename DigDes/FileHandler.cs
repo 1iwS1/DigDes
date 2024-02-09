3e0feb541fc798fc03fed5ab8f700fc2997dfb37
iwS <msp1108@mail.ru>
2024-02-10 00:04:59 +0300

    add OpenFile

---
+++ DigDes/FileHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DigDes
+{
+  public class FileHandler
+  {
+    public FileStream OpenFile(string filename)
+    {
+      FileStream stream = File.Open(filename, FileMode.Open);
+      return stream;
+    }
+
+    public 
+  }
+}
